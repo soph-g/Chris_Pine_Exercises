381a4c372f0b251db981ff776351d07d7781e77d
soph-g <sophie-gill@hotmail.com>
2016-06-06 14:54:50 +0100

    Add files via upload
    Added my answers to the exercises in lesson 1

---
+++ lesson_1.rb
@@ -0,0 +1,69 @@
+=begin
+
+	#Practice 1
+
+	puts 1 + 2
+
+=end
+
+=begin
+
+	#Practice 2
+
+	puts 1.0 + 2.0
+	puts 2.0 + 3.0
+	puts 5.0 - 8.0
+	puts 9.0 / 2.0
+
+=end
+
+=begin
+
+	#Practice 3
+
+	puts 1 + 2
+	puts 2 + 3
+	puts 5 - 8
+	puts 9 / 2
+
+=end
+
+=begin
+
+	#Exercise 1 - hours in a year
+
+	puts 365 * 24
+
+=end
+
+=begin
+
+	# Exercise 2 - minutes in a decade
+
+	puts 10 * 365 * 24 * 60
+
+=end
+
+=begin
+
+	#Exercise 3 - my age in seconds
+
+	puts (31 * 365 * 24 * 60 * 60) + ((365-35)*24*60*60)
+
+=end
+
+=begin
+
+	#Exercise 4 - how many chocolates I hope to eat (one a day, presuming I live until 90)
+
+	puts (90-31)*365
+
+=end
+
+=begin
+
+	# Exercise 5 - 1246 million seconds, how old?
+	puts (((1246000000/60)/60)/24)/365
+
+=end
+

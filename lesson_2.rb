445ae261f7faa086ce7abf608b3e1cd4a65c2ba1
Sophie Gill <sophie-gill@hotmail.com>
2016-06-07 20:50:02 +0100

    Adding Lesson 2

---
+++ lesson_2.rb
@@ -0,0 +1,26 @@
+=begin
+	
+	# Practice 1
+	puts "Hello, world!"
+	puts " "
+	puts "Good-bye."
+
+=end
+
+=begin
+
+	# Practice 2
+
+	puts "I like " + "apple pie."
+	puts "blink " * 4
+	puts 12 + 12
+	puts '12' + '12'
+	puts '12 + 12'
+	puts 2 * 5
+	puts '2' * 5
+	puts '2 * 5'
+	puts "You\'re swell!"
+
+=end
+
+

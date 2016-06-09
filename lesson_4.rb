a14da639050d7b94fe434bab9f3b7a9acb16121b
Sophie Gill <sophie-gill@hotmail.com>
2016-06-09 19:19:11 +0100

    Adding lesson 4

---
+++ lesson_4.rb
@@ -0,0 +1,23 @@
+=begin
+
+  #Exercise 1
+  puts "Hi, what is your first name?"
+  first_name = gets.chomp
+  puts "Thanks, and what is your middle name?"
+  middle_name = gets.chomp
+  puts "And finally, what is you last name?"
+  last_name = gets.chomp
+  puts " "
+  puts "Pleased to meet you " + first_name + " " + middle_name + " " + last_name + "!"
+
+=end
+
+=begin
+
+  #Exercise 2
+  puts "What is your favourite number?"
+  favourite_number = gets.chomp
+  new_favourite_number = favourite_number.to_i + 1
+  puts "May I suggest a BIGGER and BETTER number: " + new_favourite_number.to_s
+
+=end

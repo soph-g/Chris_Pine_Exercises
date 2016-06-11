e3148162e607ab6c588173f96dca12f116a626ac
Sophie Gill <sophie-gill@hotmail.com>
2016-06-11 17:53:14 +0100

    Adding lesson 7 exercise 3

--- lesson_7.rb
+++ lesson_7.rb
@@ -1,12 +1,5 @@
-#=begin
+=begin
   #Exercise 1 - Alphabetize words
-
-  #Ask the user for input
-  #while input is not blank:
-    #get input from the user, adding each input to an array
-  #sort the array alphabetically
-  #tell the user I am going to puts the array
-  #puts the sorted array
 
   puts
   puts "Please give me some words to sort, pressing enter after every word."
@@ -21,8 +14,31 @@
   end
 
   array.sort!
-
   puts "Here are you words, sorted alphabeically:"
   puts array
+
+=end
+
+=begin
+  #Exercise 2 - sorting an array without using sort.
+
+    got stuck, will come back to this!
+    
+
+=end
+
+#=begin
+  #Exercise 3 - Table of Contents
+
+  contents_table = ["Table of Contents", "Chapter 1: Numbers", "page 1", "Chapter 2: Letters", "page 72", "Chapter 3: Variables", "page 118"]
+
+  line_width = 60
+
+  puts
+  puts contents_table[0].center(line_width)
+  puts
+  puts contents_table[1].ljust(line_width/2) + contents_table[2].rjust(line_width/2)
+  puts contents_table[3].ljust(line_width/2) + contents_table[4].rjust(line_width/2)
+  puts contents_table[5].ljust(line_width/2) + contents_table[6].rjust(line_width/2)
 
 #=end

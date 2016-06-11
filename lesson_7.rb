bc3bcedc29d35937c2e0503183d9cbc241aab5fa
Sophie Gill <sophie-gill@hotmail.com>
2016-06-11 18:33:00 +0100

    Updating lesson 7 exercises.

--- lesson_7.rb
+++ lesson_7.rb
@@ -10,24 +10,67 @@
 
   while text != ""
     text = gets.chomp
-    array.push(text.downcase)
+    if text != ""
+      array.push(text.downcase)
+    end
   end
 
   array.sort!
-  puts "Here are you words, sorted alphabeically:"
+  puts "Here are your words, sorted alphabeically:"
   puts array
 
 =end
 
-=begin
+#=begin
   #Exercise 2 - sorting an array without using sort.
+  #Struggling with this one, will come back to it.
 
-    got stuck, will come back to this!
-    
+    puts
+    puts "Please give me some words to sort, pressing enter after every word."
+    puts "When you've typed enough words, press enter again."
 
-=end
+    text = "string"
+    array = []
+    sorted_array = []
+    holding_array = []
+
+    while text != ""
+      text = gets.chomp
+      if text != ""
+        array.push(text.downcase)
+      end
+    end
 
 #=begin
+    while array.length > 0 and array.length < 3
+      if array.length == 1
+        sorted_array.push(array.pop)
+      end
+      if array.length == 2
+        if array[0] < array[1]
+          holding_array.push(array.pop)
+          sorted_array.push(array.pop)
+          sorted_array.push(holding_array.pop)
+        else
+          sorted_array.push(array.pop)
+          sorted_array.push(array.pop)
+        end
+      end
+      puts sorted_array
+    end
+
+
+
+
+
+#=end
+    puts "Here are your words, sorted alphabeically:"
+    puts array
+
+
+#=end
+
+=begin
   #Exercise 3 - Table of Contents
 
   contents_table = ["Table of Contents", "Chapter 1: Numbers", "page 1", "Chapter 2: Letters", "page 72", "Chapter 3: Variables", "page 118"]
@@ -41,4 +84,4 @@
   puts contents_table[3].ljust(line_width/2) + contents_table[4].rjust(line_width/2)
   puts contents_table[5].ljust(line_width/2) + contents_table[6].rjust(line_width/2)
 
-#=end
+=end
